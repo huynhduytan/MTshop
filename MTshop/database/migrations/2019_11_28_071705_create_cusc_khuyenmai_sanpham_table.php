ea517c26bd3340c2e06a70b9a5efc7f589469c44
huynhduytan <huynhduytan0501@gmail.com>
2019-11-28 16:14:12 +0700

    thêm dữ liệu mẫu

--- MTshop/database/migrations/2019_11_28_071705_create_cusc_khuyenmai_sanpham_table.php
+++ MTshop/database/migrations/2019_11_28_071705_create_cusc_khuyenmai_sanpham_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCuscKhuyenmaiSanphamTable extends Migration
+class CreateCuscKhuyenmai_sanphamTable extends Migration
 {
     /**
      * Run the migrations.
